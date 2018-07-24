22670216892741c032c05b10ba5b454d9a087a89
Jeremy Busk <jeremy@pyrofex.net>
2018-07-23 18:56:58 -0600

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -3,3 +3,5 @@ wget https://repo.zabbix.com/zabbix/3.4/debian/pool/main/z/zabbix-release/zabbix
 dpkg -i zabbix-release_3.4-1+stretch_all.deb
 apt update
 apt -y install zabbix-agent
+sed -i 's/^Server=.*/Server=monitor-proxies.example.com/g' /etc/zabbix/zabbix_agentd.conf
+systemctl restart zabbix-agent
