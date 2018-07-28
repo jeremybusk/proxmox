c7fcaa8d280ef0b9147ddbb26c8b78e091d404b2
Jeremy Busk <jeremy@pyrofex.net>
2018-07-28 10:37:00 -0600

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -1,3 +1,7 @@
+apt update && apt upgrade
+#pvecm add 10.1.1.12
+timedatectl set-timezone Etc/UTC
+
 cd /tmp
 wget https://repo.zabbix.com/zabbix/3.4/debian/pool/main/z/zabbix-release/zabbix-release_3.4-1+stretch_all.deb
 dpkg -i zabbix-release_3.4-1+stretch_all.deb
